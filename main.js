c00cb45a1222456ee7eaad04ce4a509c560ec27c
hai sophearith <sophearithhai@gmail.com>
2024-03-26 16:11:13 +0700

    update position of the flower

--- main.js
+++ main.js
@@ -4,3 +4,12 @@ onload = () => {
       clearTimeout(c);
     }, 100);
   };
+  if (screen.width < 600) { 
+
+    document.write('<link href="style.css" type="text/css" rel="stylesheet"/>');
+    
+    } else { 
+    
+    document.write('<link href="style.css" type="text/css" rel="stylesheet"/>');
+    
+    } 
